b145c9f418247ed4e2fe64cb259b7a63f119187c
vivek <vivekthakur2128@gmail.com>
2024-08-25 21:32:47 +0530

    links-list created  using json file

---
+++ links-list/script.js
@@ -0,0 +1,21 @@
+function fetchJSONData() {
+  fetch("links.json")
+    .then((response) => response.json())
+    .then((data) => displayData(data));
+}
+fetchJSONData();
+
+function displayData(data) {
+  let ulList = document.querySelector("ul");
+  let objLength = Object.keys(data).length;
+
+  for (let index = 0; index < objLength; index++) {
+    let listItem = document.createElement("li");
+    let anchor = document.createElement("a");
+    let title = Object.keys(data)[index];
+    anchor.innerHTML = title;
+    anchor.href = Object.values(data)[index];
+    listItem.appendChild(anchor);
+    ulList.appendChild(listItem);
+  }
+}
