1a7f944816442b49c230efa31cf2a63f48570288
vivek <vivekthakur2128@gmail.com>
2024-09-19 20:29:18 +0530

    links created and set anchor href attribute

--- links-list/script.js
+++ links-list/script.js
@@ -1,21 +1,40 @@
 function fetchJSONData() {
   fetch("links.json")
     .then((response) => response.json())
-    .then((data) => displayDataAsLink(data));
+    .then((jsonData) => displayDataAsLinks(jsonData));
 }
 fetchJSONData();
 
-function displayDataAsLink(data) {
-  let ulList = document.querySelector("ul");
-  let objLength = Object.keys(data).length;
-
-  for (let index = 0; index < objLength; index++) {
-    let listItem = document.createElement("li");
-    let anchor = document.createElement("a");
-    let title = Object.keys(data)[index];
-    anchor.innerHTML = title;
-    anchor.href = Object.values(data)[index];
-    listItem.appendChild(anchor);
-    ulList.appendChild(listItem);
+function displayDataAsLinks(data) {
+  let normalLinksContainer = document.querySelector(".normalLinksDiv");
+  let intradayLinksContainer = document.querySelector(".intradaylinksdiv");
+  let shortTermLinksContainer = document.querySelector(".shortTermLinksDiv");
+  let longTermLinksContainer = document.querySelector(".longTermLinksDiv");
+  let dataLength = Object.getOwnPropertyNames(data).length; // required for terminating condition of outer loop
+  for (let outerIndex = 0; outerIndex < dataLength; outerIndex++) {
+    let subObjectName = Object.getOwnPropertyNames(data)[outerIndex];
+    let subObjectLength = Object.keys(data[`${subObjectName}`]).length; // required for terminating condition inner loop
+    for (let innerIndex = 0; innerIndex < subObjectLength; innerIndex++) {
+      let anchor = document.createElement("a");
+      anchor.style.display = "block";
+       if (!Array.isArray(data[`${subObjectName}`])) { //this line will check if direct child of data is an array or an object
+        anchor.innerHTML = Object.keys(data[`${subObjectName}`])[innerIndex];
+        anchor.href = Object.values(data[`${subObjectName}`])[innerIndex];
+        normalLinksContainer.appendChild(anchor);
+      } 
+      else {
+        anchor.innerHTML = data[`${subObjectName}`][innerIndex].title;
+        anchor.href = data[`${subObjectName}`][innerIndex].url;
+        if (subObjectName === "intraday") {
+          intradayLinksContainer.appendChild(anchor);
+        }
+        if (subObjectName === "short-term") {
+          shortTermLinksContainer.appendChild(anchor);
+        }
+        if (subObjectName === "long-term") {
+          longTermLinksContainer.appendChild(anchor);
+        }
+      }
+    }
   }
 }
