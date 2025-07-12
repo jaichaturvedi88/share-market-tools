b967c7babda2228738b1511dadb7c8e243ed5d44
Jai Chaturvedi <jai.chaturvedi88@gmail.com>
2025-07-12 11:31:26 +0530

    #48 Now we are listening the URL change. When URL changes, respective UIs are created

--- zerodha-extension/content.js
+++ zerodha-extension/content.js
@@ -1,17 +1,35 @@
 console.log('Its from content.js file')
 
-function createUi() {
-  // let url = 'https://kite.zerodha.com/positions';
-  let url = location.href;
-
+function createUi(url) {
+  const holdingsUI = document.querySelector('div.calculator');
   if (url.includes("/positions")) {
     positions.createPositionsPanel();
+    holdingsUI.remove();
   } else if (url.includes("/holdings")){
     holdings.createHoldingsUi()
   }
 }
 
 setTimeout(() => {
-  createUi(); // writing this in settimeout because sometimes window.onload won't work(in very few cases)
+  createUi(location.href); // writing this in settimeout because sometimes window.onload won't work(in very few cases)
   
 }, 3000);
+
+let currentUrl = location.href;
+
+const observer = new MutationObserver(() => {
+  if (location.href !== currentUrl) {
+    console.log("URL changed:", location.href);
+    currentUrl = location.href;
+
+    // Run your logic here
+    onUrlChange(location.href);
+  }
+});
+
+observer.observe(document, { subtree: true, childList: true });
+
+function onUrlChange(newUrl) {
+  console.log("Reacting to new URL:", newUrl);
+  createUi(newUrl);
+}
