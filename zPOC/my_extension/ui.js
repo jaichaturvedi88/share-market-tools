65f0dd62ca0eaea090dd87cfde26b5268848f428
Jai Chaturvedi <jai.chaturvedi88@gmail.com>
2026-02-22 19:18:30 +0530

    created new chartink scan viewer

--- zPOC/my_extension/ui.js
+++
@@ -1,7 +0,0 @@
-function createCountBox(count) {
-    let displayDiv = document.createElement("div");
-    displayDiv.innerText = `Word 'Bank' Count: ${count}`;
-    displayDiv.classList.add("custom-count-box");
-
-    document.body.appendChild(displayDiv);
-}
